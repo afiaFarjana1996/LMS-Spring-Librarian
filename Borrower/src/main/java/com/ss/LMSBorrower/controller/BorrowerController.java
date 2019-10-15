352ead91a45d8dfca95ca6379ee61cae565fd9a3
afiaFarjana1996 <55745983+afiaFarjana1996@users.noreply.github.com>
2019-10-15 10:43:14 -0400

    Changed mapping methods

--- Borrower/src/main/java/com/ss/LMSBorrower/controller/BorrowerController.java
+++ Borrower/src/main/java/com/ss/LMSBorrower/controller/BorrowerController.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,22 +17,23 @@ import com.ss.LMSBorrower.service.BorrowerService;
 
 @RestController
 
+@RequestMapping("lms/")
 public class BorrowerController {
 
 	@Autowired
 	BorrowerService borrowerService;
 	
-	@RequestMapping(path="lms/book-checkout/card-number/{cardNumber}/library-branch-id/{branchId}/book-id/{bookId}",method=RequestMethod.PUT)
+	@PutMapping("book-checkout/card-number/{cardNumber}/library-branch-id/{branchId}/book-id/{bookId}")
 	public ResponseEntity<String> checkOutBook(@PathVariable int cardNumber, @PathVariable int branchId, @PathVariable int bookId ){
 		return borrowerService.checkOutBook(cardNumber, branchId, bookId);
 	}
 	
-	@RequestMapping("lms/see-loaned-books/card-number/{cardNumber}")
+	@GetMapping("see-loaned-books/card-number/{cardNumber}")
 	public List<BookLoans> getListOfLoanedBooks(@PathVariable int cardNumber){
 		return borrowerService.getListOfLoanedBooks(cardNumber);
 	}
 	
-	@RequestMapping(path="lms/book-return/card-number/{cardNumber}/library-branch-id/{branchId}/book-id/{bookId}",method=RequestMethod.DELETE)
+	@DeleteMapping("book-return/card-number/{cardNumber}/library-branch-id/{branchId}/book-id/{bookId}")
 	public ResponseEntity<String> returnBook(@PathVariable int cardNumber, @PathVariable int branchId, @PathVariable int bookId ){
 		return borrowerService.returnBook(cardNumber, branchId, bookId);
 	}
